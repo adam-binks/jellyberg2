88d61eb1dcdea17a63f07f20fb9c1bead9f5a321
jellyberg2 <adam.r.binks@gmail.com>
2015-07-30 14:10:01 +0100

    added wikiScraper.py - automatically grab country areas
    OPEN DATA yo

--- wikiScraper.py
+++ wikiScraper.py
@@ -4,30 +4,42 @@ def getCountryArea(country):
 	page = wikipedia.page(country)
 
 	html = page.html().encode("utf-8")
-	tableIndex = html.index('infobox geography vcard')
-	infoTablePlus = html[tableIndex:]
-	print(infoTablePlus)  
+	# chop off all the stuff before the area
+	text = trimText(html, 'infobox geography vcard')
+	text = trimText(text, 'Area')
+	text = trimText(text, 'Total')
+
+	# then look for numbers until the end of the number is reached
+	areaString = ""
+	for char in text:
+		if char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+			# we've found the area
+			areaString += char
+		else:
+			if areaString != "" and char not in [',', ' ']:
+				# a number has been entered
+				break
+			else:
+				# no number - we're still in html syntax
+				continue
+	return int(areaString)
+
+
+def trimText(fullText, searchTerm):
+	index = fullText.lower().index(searchTerm.lower())
+	return fullText[index:]
 	
 
-
-	# content = page.content.encode("utf-8")
-	# index = page.content.index('area')
-	# index += len('area')
-	# postAreaString = content[index:index+500]
-	# print(country)
-	# print(content)
-
-	# area = ''
-	# for char in postAreaString:
-	# 	if not char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-	# 		if area == '' or char in [',', ' ', '.']:
-	# 			continue
-	# 		else:
-	# 			print(char)
-	# 			return int(area)
-	# 	else:
-	# 		area += char
+def generateJson(countries):
+	json = ',';
+	for country in countries:
+		json += '"'+country+'": ' + str(getCountryArea(country))
+		if country != countries[-1]:
+			json += ', '
+		else:
+			json += '};'
+	return json
 
 
-for country in ['russia', 'england', 'brazil', 'luxembourg']:
-	print(country + ': ' + str(getCountryArea(country)))
+json = generateJson(['russia', 'wales', 'new zealand'])
+print(json)
